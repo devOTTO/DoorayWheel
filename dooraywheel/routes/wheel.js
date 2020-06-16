624f38250189f087e378c0ea2d7cd7add6c9db83
devOTTO <yoongotto@gmail.com>
2020-06-16 10:32:37 +0900

    fix: error

--- dooraywheel/routes/wheel.js
+++ dooraywheel/routes/wheel.js
@@ -79,15 +79,15 @@ exports.call = (req, res) => {
 
 // Request URL
 exports.callNumber = (req, res) => {
-  const params = req.body.text.split(' ')
+  const params = req.body.text
 
-  if (params.length < 1 || Number(params[0]) <= 0 || params[0] === 'help') {
+  if (params.length < 1 || Number(params) <= 0 || params === 'help') {
     logger.access({ type: logger.ACCESS_TYPE.HELP, body: req.body })
     return res.status(200).send({
         text: getText(MSG.MSG_TYPE.NHELP)
     });
   }
-  if(params.length - 1 < params[0]){
+  if(params.length - 1 < params){
     logger.access({ type: logger.error, body: req.body })
     return res.status(200).send({
         text: getText(MSG.MSG_TYPE.NHELP)
@@ -95,12 +95,12 @@ exports.callNumber = (req, res) => {
   }
   logger.access({ type: logger.ACCESS_TYPE.CALL, body: req.body })
   //아닌 경우 wheel
-  const winnerNum = randomRange(1, Number(params[0]))
+  const winnerNum = randomRange(1, Number(params))
 
   let text = "돌려 돌려 돌림판~ 행운의 숫자는!"
   text += "\n☾" + winnerNum + "ఌ "
   text += "입니다!"
-  let title = "숫자 범위 : 1 ~ "+ params[0]
+  let title = "숫자 범위 : 1 ~ "+ params
 
   return res.status(200).send({
     responseType: 'inChannel',
